9fc76c2450ca0a4e7d9aa957b27b3878c3ded17e
Jisan-mia <jisanmia47@gmail.com>
2021-12-09 03:15:03 +0600

    done

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="addTask()" :style="{background: color}" class="btn"> {{text}} </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+    showAddTask: Boolean
+  },
+  methods: {
+    addTask() {
+      this.$emit('btn-click')
+    }
+  }
+}
+</script>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :key="task.id" v-for="task in tasks" >
+    <SingleTask @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task"/>
+  </div>
+</template>
+
+<script>
+import SingleTask from "./SingleTask.vue"
+
+
+export default {
+  components: { SingleTask },
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
